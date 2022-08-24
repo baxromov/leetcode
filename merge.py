87751499ec5177ceb0adf3e235ed93138adec680
baxromov <baxromov.shahzodbek@gmail.com>
2022-08-24 19:40:40 +0500

    add 3 solution to merge.py

--- merge.py
+++ merge.py
@@ -33,5 +33,48 @@ def merge(nums1: list, m: int, nums2: list, n: int):
     nums1.sort()
 
 
+print(nums1)
 merge(nums1, m,nums2,n )
 print(nums1)
+
+
+def merge(self, nums1: int, m: int, nums2: int, n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    i = 0
+    while i < len(nums1):
+        if nums1.count(0) and len(nums2) > 0:
+            indexx = nums1.index(0)
+            nums1[indexx] = nums2[0]
+            nums2.pop(0)
+        i += 1
+    nums1.sort()
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+nums1[m] = 
+"""
+
+
+
+
+
+
+
--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,4 +1,4 @@
-nums = [1, 1, 2]
+nums = [0, 0, 1, 1, 1, 2, 2 ,3 ,3, 4]
 
 
 def removeDuplicates(nums):
@@ -11,3 +11,15 @@ def removeDuplicates(nums):
             i += 1
             nums[i] = nums[j]
     return i + 1
+
+print(removeDuplicates(nums))
+
+"""
+
+[0, 0, 1, 1, 1, 2, 2 ,3 ,3, 4]
+i = 0 , j = 2
+i = 1, j = 3
+num[i] = 1
+
+
+"""
