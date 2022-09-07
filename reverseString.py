3dba09207fc0e2cf8e79cb85b1f241f6ccfbd130
baxromov <baxromov.shahzodbek@gnail.com>
2022-09-07 16:45:32 +0500

    add

---
+++ reverseString.py
@@ -0,0 +1,24 @@
+s = ["h","e","l","l","o"]
+
+
+def reverseString1(s):
+    l, r = 0, len(s) - 1
+    while l < r:
+        s[l], s[r] = s[r], s[l]
+        l, r = l + 1, r - 1
+
+
+def reverseString2(s):
+    stack = []
+    for c in s:
+        stack.append(c)
+    i = 0
+    while stack:
+        s[i] = stack.pop()
+        i += 1
+
+
+
+# reverseString1(s)
+reverseString2(s)
+print(s)
